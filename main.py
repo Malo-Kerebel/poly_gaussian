a8a4f9a33929a9d5d79f4300a2fcdb23f6ec7d68
Malo Kerebel <malo.kerebel@laposte.net>
2022-06-24 15:28:02 +0200

    move main code to functions making it easier to test

--- main.py
+++ main.py
@@ -48,17 +48,196 @@ def merge_array(mu, sigma, coeff):
     return ret
 
 
+def gen_test(test_random=False, rand_B=False, rand_D=False, B = 2, percent_D = 0.75,):
+
+    if rand_B:
+        B = rand_range(1.5, 3.5)  # Champ magnétique en tesla
+
+    if rand_D:
+        percent_D = rand_range(0.55, 0.95)  # Pourcentage de deutérium dans la valeur de test (uniquement si test_random == False)
+    
+    if test_random:
+
+        mu = []
+        sigma = []
+        coeff = []
+
+        for j in range(n_gauss):
+            mu.append(rand_range(-3, 3))
+            sigma.append(rand_range(0.5, 2))
+            coeff.append(rand_range(0.5, 3))
+
+        courbe_test = poly_gauss(x, mu, sigma, coeff, noise_test)
+        return courbe_test.y, merge_array(mu, sigma, coeff), None, None
+
+    else:
+
+        y_test, expected = Gaussian(percent_D, B, noise=noise_test)
+        return y_test, expected, B, percent_D
+
+
+def test(y_test, expected, B=None, percent_D=None):
+    
+    result_NN = model_NN.predict(y_test.reshape(1, -1))[0]
+
+    if train_random:
+        mu_NN = result_NN[:n_gauss]
+        sigma_NN = abs(result_NN[n_gauss:2*n_gauss])
+        coeff_NN = result_NN[2*n_gauss:]
+    else:
+        mu_NN = result_NN[:n_gauss]
+        sigma_NN = abs(result_NN[n_gauss:2+n_gauss])
+        coeff_NN = result_NN[2+n_gauss:]
+
+    print(result_NN)
+    print(expected)
+
+    if sk_learn:
+
+        model_sk = model
+
+        t1 = time()
+
+        model_sk.fit(data, target_full)#, validation_split=0.2)
+
+        t2 = time()
+
+        print("fini fit full, in", t2-t1)
+
+        # result_sk = model_sk.predict(courbe_test.y.reshape(1, -1))[0]
+        result_sk = model_sk.predict(y_test.reshape(1, -1))[0]
+
+        mu_sk = result_sk[:n_gauss]
+        sigma_sk = abs(result_sk[n_gauss:2*n_gauss])
+        coeff_sk = result_sk[2*n_gauss:3*n_gauss]
+
+        print(result_sk)
+        if test_random:
+            print(merge_array(courbe_test.mu, courbe_test.sigma, courbe_test.coeff))
+
+
+    if test_random:
+
+        plt.plot(x_show, courbe_test.y, "b", label="somme des courbes originelles")
+        for i in range(n_gauss):
+            if i == 0:
+                plt.plot(x_show, gaussienne(courbe_test.x, courbe_test.mu[i],
+                                            courbe_test.sigma[i], courbe_test.coeff[i]),
+                         "b,", label="Courbe originelles")
+            else:
+                plt.plot(x_show, gaussienne(courbe_test.x, courbe_test.mu[i],
+                                            courbe_test.sigma[i], courbe_test.coeff[i]),
+                         "b,")
+
+        resultat = poly_gauss(courbe_test.x, mu_NN, sigma_NN, coeff_NN)
+        plt.plot(resultat.x, resultat.y, "g--", label="Somme des courbes prédites, réseau neuronal")
+
+        for i in range(n_gauss):
+            if i > 0:
+                plt.plot(x_show, gaussienne(courbe_test.x, mu_NN[i],
+                                            sigma_NN[i], coeff_NN[i]),
+                         "g,")
+            else:
+                plt.plot(x_show, gaussienne(courbe_test.x, mu_NN[i],
+                                            sigma_NN[i], coeff_NN[i]),
+                         "g,", label="Courbe predites, réseau neuronal")
+
+        if sk_learn:
+            resultat = poly_gauss(courbe_test.x, mu_sk, sigma_sk, coeff_sk)
+            plt.plot(x_show, resultat.y, "r--", label="Somme des courbes prédites, scickit learn")
+
+            for i in range(n_gauss):
+                if i == 0:
+                    plt.plot(x_show, gaussienne(courbe_test.x, mu_sk[i],
+                                                sigma_sk[i], coeff_sk[i]),
+                             "r,", label="Courbe predites, scickit learn")
+                else:
+                    plt.plot(x_show, gaussienne(courbe_test.x, mu_sk[i],
+                                                sigma_sk[i], coeff_sk[i]),
+                             "r,")
+
+    else:
+
+        # x = change_x(x)
+        # mu_full = change_x(mu_full)
+        # if sk_learn:
+        #     result_mu = change_x(result_mu)
+
+        plt.plot(x_show, y_test, "k", label="somme des courbes originelles")
+
+        resultat_D = poly_gauss(x, mu_NN[:2], [sigma_NN[0], sigma_NN[0]],
+                                [coeff_NN[0], coeff_NN[0]])
+        resultat_H = poly_gauss(x, mu_NN[2:], [sigma_NN[1], sigma_NN[1]],
+                                [coeff_NN[1], coeff_NN[1]])
+
+        if save_txt:
+            with open("B="+str(B)+",D="+str(percent_D)+"N="+str(N)+".txt", 'w') as f:
+                f.write("# y_true, y_H, y_D\n")
+                for i in range(n):
+                    tmp = f"{y_test[i]} {resultat_H.y[i]} {resultat_D.y[i]}\n"
+                    f.write(tmp)
+
+        plt.plot(x_show, resultat_D.y, "r--", label="Somme des courbes prédites pour D, réseau neuronal")
+        plt.plot(x_show, resultat_H.y, "b--", label="Somme des courbes prédites pour H, réseau neuronal")
+
+        for i in range(2):
+            if i > 0:
+                plt.plot(x_show, gaussienne(x, mu_NN[i],
+                                            sigma_NN[0], coeff_NN[0]),
+                         "r,")
+            else:
+                plt.plot(x_show, gaussienne(x, mu_NN[i],
+                                            sigma_NN[0], coeff_NN[0]),
+                         "r,", label="Courbe predites pour D, réseau neuronal")
+
+        for i in range(2, n_gauss):
+            if i > 2:
+                plt.plot(x_show, gaussienne(x, mu_NN[i],
+                                            sigma_NN[1], coeff_NN[1]),
+                         "b,")
+            else:
+                plt.plot(x_show, gaussienne(x, mu_NN[i],
+                                            sigma_NN[1], coeff_NN[1]),
+                         "b,", label="Courbe predites pour H, réseau neuronal")
+
+        if sk_learn:
+            resultat = poly_gauss(x, mu_sk, sigma_sk, coeff_sk)
+            plt.plot(resultat.x, resultat.y, "r--", label="Somme des courbes prédites, scickit learn")
+
+            for i in range(n_gauss):
+                if i == 0:
+                    plt.plot(x_show, gaussienne(x, mu_sk[i],
+                                                sigma_sk[i], coeff_sk[i]),
+                             "r,", label="Courbe predites, scickit learn")
+                else:
+                    plt.plot(x_show, gaussienne(x, mu_sk[i],
+                                                sigma_sk[i], coeff_sk[i]),
+                             "r,")
+
+        rapport_isotopique = resultat_H.int() / (resultat_H.int() + resultat_D.int()) * 100
+        print("Concentration isotopique :", rapport_isotopique, "% d'H, valeur théorique ", (1-percent_D)*100, "%")
+
+        error = abs(rapport_isotopique-(1-percent_D)*100)/((1-percent_D)*100)*100
+        print("Erreur de :", error, '%')
+
+        if save_png:
+            plt.savefig("B="+str(B)+",D="+str(percent_D)+",N="+str(N)+",error="+str(error)+".png")
+
+    plt.legend()
+    plt.show()
+
+
 sk_learn = False  # Est-ce que du ML doit être utilisé
 train_random = False  # Est-ce que les valeurs de test doivent être aléatoire ou
 # utilisé les contraintes supplémentaires
 test_random = False   # Est-ce que la valeur de test doit être aléatoire ou
 # venir de données synthétique
 
-N = 500000  # Nombre de valeurs d'entrainement
+N = 100000  # Nombre de valeurs d'entrainement
 n = 1000    # Nombre de points dans les courbes d'entrainements
 n_gauss = 4  # nombre de gaussienne à additionner
 
-n_epochs = 10   # Nombre d'épochs sur lesquel le NN doit itérer
+n_epochs = 32   # Nombre d'épochs sur lesquel le NN doit itérer
 
 x_min = -10  # Valeurs minimales et maximales du x des données
 x_max = 10
@@ -66,16 +245,13 @@ x_max = 10
 lambda_min = 6558  # Valeurs minimales des longueurs d'ondes étudier
 lambda_max = 6565
 
-percent_D = 0.75  # Pourcentage de deutérium dans la valeur de test (uniquement si test_random == False)
-B = 2.5           # Champ magnétique en tesla
-
 noise_train = True
 noise_test = True
 
 show = False
 
-save_txt = True
-save_png = True
+save_txt = False
+save_png = False
 
 x = np.linspace(x_min, x_max, n)
 x_show = np.linspace(lambda_min, lambda_max, n)
@@ -234,172 +410,6 @@ model_NN.fit(data, target_full, epochs=n_epochs)#, validation_split=0.2)
 t2 = time()
 print("fini fit NN, in", t2-t1)
 
-if test_random:
+y_test, expected, B, percent_D = gen_test(False, True, True)
 
-    mu = []
-    sigma = []
-    coeff = []
-
-    for j in range(n_gauss):
-        mu.append(rand_range(-3, 3))
-        sigma.append(rand_range(0.5, 2))
-        coeff.append(rand_range(0.5, 3))
-
-    courbe_test = poly_gauss(x, mu, sigma, coeff, noise_test)
-
-else:
-
-    y_test, expected = Gaussian(percent_D, B, noise=noise_test)
-
-if test_random:
-    result_NN = model_NN.predict(courbe_test.reshape(1, -1))[0]
-else:
-    result_NN = model_NN.predict(y_test.reshape(1, -1))[0]
-
-if train_random:
-    mu_NN = result_NN[:n_gauss]
-    sigma_NN = abs(result_NN[n_gauss:2*n_gauss])
-    coeff_NN = result_NN[2*n_gauss:]
-else:
-    mu_NN = result_NN[:n_gauss]
-    sigma_NN = abs(result_NN[n_gauss:2+n_gauss])
-    coeff_NN = result_NN[2+n_gauss:]
-
-print(result_NN)
-if test_random:
-    print(merge_array(courbe_test.mu, courbe_test.sigma, courbe_test.coeff))
-else:
-    print(expected)
-
-if sk_learn:
-
-    model_sk = model
-
-    t1 = time()
-
-    model_sk.fit(data, target_full)#, validation_split=0.2)
-
-    t2 = time()
-
-    print("fini fit full, in", t2-t1)
-
-    # result_sk = model_sk.predict(courbe_test.y.reshape(1, -1))[0]
-    result_sk = model_sk.predict(y_test.reshape(1, -1))[0]
-
-    mu_sk = result_sk[:n_gauss]
-    sigma_sk = abs(result_sk[n_gauss:2*n_gauss])
-    coeff_sk = result_sk[2*n_gauss:3*n_gauss]
-
-    print(result_sk)
-    if test_random:
-        print(merge_array(courbe_test.mu, courbe_test.sigma, courbe_test.coeff))
-
-
-if test_random:
-
-    plt.plot(x_show, courbe_test.y, "b", label="somme des courbes originelles")
-    for i in range(n_gauss):
-        if i == 0:
-            plt.plot(x_show, gaussienne(courbe_test.x, courbe_test.mu[i],
-                                               courbe_test.sigma[i], courbe_test.coeff[i]),
-                     "b,", label="Courbe originelles")
-        else:
-            plt.plot(x_show, gaussienne(courbe_test.x, courbe_test.mu[i],
-                                               courbe_test.sigma[i], courbe_test.coeff[i]),
-                     "b,")
-
-    resultat = poly_gauss(courbe_test.x, mu_NN, sigma_NN, coeff_NN)
-    plt.plot(resultat.x, resultat.y, "g--", label="Somme des courbes prédites, réseau neuronal")
-
-    for i in range(n_gauss):
-        if i > 0:
-            plt.plot(x_show, gaussienne(courbe_test.x, mu_NN[i],
-                                               sigma_NN[i], coeff_NN[i]),
-                     "g,")
-        else:
-            plt.plot(x_show, gaussienne(courbe_test.x, mu_NN[i],
-                                               sigma_NN[i], coeff_NN[i]),
-                     "g,", label="Courbe predites, réseau neuronal")
-
-    if sk_learn:
-        resultat = poly_gauss(courbe_test.x, mu_sk, sigma_sk, coeff_sk)
-        plt.plot(x_show, resultat.y, "r--", label="Somme des courbes prédites, scickit learn")
-
-        for i in range(n_gauss):
-            if i == 0:
-                plt.plot(x_show, gaussienne(courbe_test.x, mu_sk[i],
-                                                   sigma_sk[i], coeff_sk[i]),
-                         "r,", label="Courbe predites, scickit learn")
-            else:
-                plt.plot(x_show, gaussienne(courbe_test.x, mu_sk[i],
-                                                   sigma_sk[i], coeff_sk[i]),
-                         "r,")
-
-else:
-
-    # x = change_x(x)
-    # mu_full = change_x(mu_full)
-    # if sk_learn:
-    #     result_mu = change_x(result_mu)
-
-    plt.plot(x_show, y_test, "k", label="somme des courbes originelles")
-
-    resultat_D = poly_gauss(x, mu_NN[:2], [sigma_NN[0], sigma_NN[0]],
-                            [coeff_NN[0], coeff_NN[0]])
-    resultat_H = poly_gauss(x, mu_NN[2:], [sigma_NN[1], sigma_NN[1]],
-                            [coeff_NN[1], coeff_NN[1]])
-
-    if save_txt:
-        with open("B="+str(B)+",D="+str(percent_D)+"N="+str(N)+".txt", 'w') as f:
-            f.write("# y_true, y_H, y_D\n")
-            for i in range(n):
-                tmp = f"{y_test[i]} {resultat_H.y[i]} {resultat_D.y[i]}\n"
-                f.write(tmp)
-
-    plt.plot(x_show, resultat_D.y, "r--", label="Somme des courbes prédites pour D, réseau neuronal")
-    plt.plot(x_show, resultat_H.y, "b--", label="Somme des courbes prédites pour H, réseau neuronal")
-
-    for i in range(2):
-        if i > 0:
-            plt.plot(x_show, gaussienne(x, mu_NN[i],
-                                   sigma_NN[0], coeff_NN[0]),
-                     "r,")
-        else:
-            plt.plot(x_show, gaussienne(x, mu_NN[i],
-                                   sigma_NN[0], coeff_NN[0]),
-                     "r,", label="Courbe predites pour D, réseau neuronal")
-
-    for i in range(2, n_gauss):
-        if i > 2:
-            plt.plot(x_show, gaussienne(x, mu_NN[i],
-                                   sigma_NN[1], coeff_NN[1]),
-                     "b,")
-        else:
-            plt.plot(x_show, gaussienne(x, mu_NN[i],
-                                   sigma_NN[1], coeff_NN[1]),
-                     "b,", label="Courbe predites pour H, réseau neuronal")
-
-    if sk_learn:
-        resultat = poly_gauss(x, mu_sk, sigma_sk, coeff_sk)
-        plt.plot(resultat.x, resultat.y, "r--", label="Somme des courbes prédites, scickit learn")
-
-        for i in range(n_gauss):
-            if i == 0:
-                plt.plot(x_show, gaussienne(x, mu_sk[i],
-                                       sigma_sk[i], coeff_sk[i]),
-                         "r,", label="Courbe predites, scickit learn")
-            else:
-                plt.plot(x_show, gaussienne(x, mu_sk[i],
-                                       sigma_sk[i], coeff_sk[i]),
-                         "r,")
-
-    rapport_isotopique = resultat_H.int() / (resultat_H.int() + resultat_D.int()) * 100
-    print("Concentration isotopique :", rapport_isotopique, "% d'H, valeur théorique ", (1-percent_D)*100, "%")
-
-if save_png:
-    error = abs(rapport_isotopique-(1-percent_D)*100)/((1-percent_D)*100)*100
-    print("Erreur de :", error)
-    plt.savefig("B="+str(B)+",D="+str(percent_D)+",N="+str(N)+",error="+str(error)+".png")
-
-plt.legend()
-plt.show()
+test(y_test, expected, B, percent_D)
